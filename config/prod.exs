dc757f55f66400af3e60de9ac7d1ee68ddc0c242
jmn <norlund@gmail.com>
2019-09-27 16:03:55 +0200

    socket host

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :phx, PhxWeb.Endpoint,
-  url: [host: "postya.net", port: 80],
+  url: [host: "www.postya.net", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
