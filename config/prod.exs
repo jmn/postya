91237f768da8538e0ebdf61cc504e1ffb294a4d9
jmn <norlund@gmail.com>
2019-09-28 08:55:37 +0200

    force ssl, change host

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ use Mix.Config
 config :phx, PhxWeb.Endpoint,
   url: [host: nil],
   #  url: [host: "www.postya.net", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
