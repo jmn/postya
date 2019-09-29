b90c7acfbc350f10e83bc0a9f964200ebbe53f9f
jmn <norlund@gmail.com>
2019-09-29 18:00:47 +0200

    disable force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,9 @@ config :phx, PhxWeb.Endpoint,
   url: [host: "www.postya.net", port: 80],
   # url: [host: nil],
   canonical_host: "www.postya.net",
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  #force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  cache_static_manifest: "priv/static/cache_manifest.json"
+
+# force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
