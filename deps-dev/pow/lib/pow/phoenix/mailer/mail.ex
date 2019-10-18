91cf7d8474b9d1af3f8c527ac1b9d5e2933e56d6
Jussi Norlund <norlund@gmail.com>
2019-10-18 02:46:52 +0200

    go

--- deps-dev/pow/lib/pow/phoenix/mailer/mail.ex
+++ deps-dev/pow/lib/pow/phoenix/mailer/mail.ex
@@ -53,9 +53,8 @@ defmodule Pow.Phoenix.Mailer.Mail do
     config       = Plug.fetch_config(conn)
     web_module   = Config.get(config, :web_mailer_module)
     view_assigns = Keyword.merge([conn: conn, user: user], assigns)
-    view_module  = nil #ViewHelpers.build_view_module(view_module, web_module)
-
-    subject = render_subject(view_module, template, view_assigns)
+    view_module  = ViewHelpers.build_view_module(view_module, web_module)
+    raise render_subject(view_module, template, view_assigns)
     Logger.debug("SUBJECT")
     text    = "TEXT" #render(view_module, template, conn, view_assigns, :text)
     Logger.debug("TEXT")
