7c47787b0220a2ccefd2a8ec2b198eb6913d09a6
jmn <norlund@gmail.com>
2019-11-10 19:52:33 +0100

    modify users

--- lib/phx/users/user.ex
+++ lib/phx/users/user.ex
@@ -12,13 +12,7 @@ defmodule Phx.Users.User do
     timestamps()
   end
 
-  def changeset(user_or_changeset, attrs) do
-    user_or_changeset
-    |> pow_changeset(attrs)
-    |> changeset_role(attrs)
-    |> pow_extension_changeset(attrs)
-  end
-
+  @spec changeset_role(Ecto.Schema.t() | Ecto.Changeset.t(), map()) :: Ecto.Changeset.t()
   def changeset_role(user_or_changeset, attrs) do
     user_or_changeset
     |> Ecto.Changeset.cast(attrs, [:role])
