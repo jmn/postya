3f976ea940911a39538691c56250de7e42212fd7
jmn <norlund@gmail.com>
2020-05-02 19:15:04 +0200

    drop constraint

--- priv/repo/migrations/20200502094823_delete.exs
+++ priv/repo/migrations/20200502094823_delete.exs
@@ -6,7 +6,7 @@ defmodule Phx.Repo.Migrations.Delete do
     drop(constraint(:fd_feed_tags, :fd_feed_tags_tag_id_fkey))
 
     alter table(:fd_feed_tags) do
-      modify(:fd_feed_id, references(:fd_feed, on_delete: :delete_all), primary_key: true)
+      modify(:fd_feed_id, references(:fd_feed, on_delete: :delete_all))
       modify(:fd_tag_id, references(:fd_tags, on_delete: :delete_all), primary_key: true)
     end
   end
