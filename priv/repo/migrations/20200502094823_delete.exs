6163a516dcf0cc89fd2fa4c3e534f479ea0ab887
jmn <norlund@gmail.com>
2020-05-02 19:11:15 +0200

    drop constraint

--- priv/repo/migrations/20200502094823_delete.exs
+++ priv/repo/migrations/20200502094823_delete.exs
@@ -2,8 +2,8 @@ defmodule Phx.Repo.Migrations.Delete do
   use Ecto.Migration
 
   def change do
-   # drop(constraint(:fd_feed_tags, :fd_feed_tags_fd_feed_id_fkey))
-    #drop(constraint(:fd_feed_tags, :fd_feed_tags_tag_id_fkey))
+    drop(constraint(:fd_feed_tags, :fd_feed_tags_fd_feed_id_fkey))
+    drop(constraint(:fd_feed_tags, :fd_feed_tags_tag_id_fkey))
 
     alter table(:fd_feed_tags) do
       modify(:fd_feed_id, references(:fd_feed, on_delete: :delete_all), primary_key: true)
