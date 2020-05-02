f00ad96c49e49ec1023db4019a97b79d86b1839e
jmn <norlund@gmail.com>
2020-05-02 19:41:27 +0200

    select distinct tags

--- lib/phx_web/views/layout_view.ex
+++ lib/phx_web/views/layout_view.ex
@@ -7,6 +7,7 @@ defmodule PhxWeb.LayoutView do
       from t in FDTag,
         left_join: up in assoc(t, :fd_feed),
         preload: :fd_feed,
+        distinct: true,
         select: t,
         where: fragment("? IN (SELECT fd_tag_id FROM fd_feed_tags)", t.id)
     )
