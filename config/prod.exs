892535077fd3d9cca08a3a7cefb85c8ede7cf050
jmn <norlund@gmail.com>
2020-09-27 18:40:08 +0200

    prod config

--- config/prod.exs
+++ config/prod.exs
@@ -10,9 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :phx, PhxWeb.Endpoint,
-  url: [host: "www.postya.net", port: 80],
+  url: [host: nil],
   # url: [host: nil],
-  canonical_host: "www.postya.net",
+  #canonical_host: "www.postya.net",
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # force_ssl: [rewrite_on: [:x_forwarded_proto]]
